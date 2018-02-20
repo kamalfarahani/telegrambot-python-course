d16a0a6718edfe0233c2ca196da8bc223c7fb940
Kamal Max <pmer93@gmail.com>
2018-02-20 21:04:15 +0330

    keyboard bot added

---
+++ keyboardbot.py
@@ -0,0 +1,33 @@
+import logging
+from telegram import ReplyKeyboardMarkup, KeyboardButton
+from telegram.ext import Updater, CommandHandler
+
+
+def make_keyboard(bot, update):
+    custom_keyboard = [[KeyboardButton(text='hello')], 
+                        [KeyboardButton(text='bye')]]
+    
+    kb_markup = ReplyKeyboardMarkup(custom_keyboard)
+
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text='Making keyboard', 
+        reply_markup=kb_markup)
+    
+
+def main():
+    updater = Updater(token='TOKEN')
+    dispacher = updater.dispatcher
+
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO)
+
+    make_keyboard_handler = CommandHandler('keyboard', make_keyboard)
+    dispacher.add_handler(make_keyboard_handler)
+
+    updater.start_polling()
+
+
+if __name__ == '__main__':
+    main()
