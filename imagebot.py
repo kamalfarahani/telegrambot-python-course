14cd219039f9c79e7f75a71d02e6125718e6a651
Kamal Max <pmer93@gmail.com>
2018-02-20 20:11:20 +0330

    beautify

--- imagebot.py
+++ imagebot.py
@@ -3,7 +3,8 @@ from telegram.ext import Updater, CommandHandler
 
 def send_photo(bot, update):
     bot.send_photo(
-        chat_id=update.message.chat_id, photo=open('./Tree.jpg', 'rb'))
+        chat_id=update.message.chat_id, 
+        photo=open('./Tree.jpg', 'rb'))
 
 
 def main():
